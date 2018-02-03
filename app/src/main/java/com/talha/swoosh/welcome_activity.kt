4bde34c416615956d5c31a0f0e12bdc13c9be6b6
Talha Rafique <talha_rafiq123@yahoo.com>
2018-02-03 09:56:41 +0500

    Initial commit

---
+++ app/src/main/java/com/talha/swoosh/welcome_activity.kt
@@ -0,0 +1,13 @@
+package com.talha.swoosh
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class welcome_activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+
+}
